4d7cb5fc43ab2268d6ce69924d0ef30c7f3dbb53
LXH <lxh@LXHdeMacBook-Pro.local>
2017-05-28 17:12:01 -0700

    Revised 28 May

--- Power_Law_Distribute.py
+++ Power_Law_Distribute.py
@@ -1,8 +1,9 @@
 import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 from math import exp, factorial
-from scipy.stats import rv_continuous, rv_discrete, poisson, describe, histogram
+from scipy.stats import rv_continuous, rv_discrete, poisson, describe, histogram, randint
 
 
 class power_gen(rv_continuous):
@@ -48,12 +49,50 @@ def bi_degree(a, d, alpha, c, n):
 def stat_test( arry ):
     print(describe(arry))
     plt.hist(arry, bins = 'auto')
-    plt.tittle("Degree Histogram")
+    plt.title("Degree Histogram")
     plt.show()
 
-seq = bi_degree(1, 1.5, 2, 2, 10000)
-stat_test( seq[0] )
-stat_test( seq[1] )
+def Directed_gen(a, d, alpha, c, n):
+    """
+    a function that generates same-sum sample pair of bidegree using the algorithm
+    the function returns a tuple  (in-degree, out-degree)
+
+    Input Parameters
+    -----------------
+    a : coefficient
+    d : power term
+    alpha : power for W+ CDF
+    c : W- ceiling
+    n: simulation sample number
+
+    """
+    beta = alpha * d
+    # derive k
+    k = min(1 - 1/alpha, 1 - 1/beta, 1/2)
+    delta0 = k/2  # take k/2 as fixed
+
+    Delta_n = sum(bi_degree(a, d, alpha, c, n)[0]) - sum(bi_degree(a, d, alpha, c, n)[1])
+
+    # repeat until Delta_n is small enough to be "negligible"
+    while Delta_n > n**(1 - k + delta0):
+        samesum_bi_degree = bi_degree(a, d, alpha, c, n)
+        Delta_n = sum(samesum_bi_degree[0]) - sum(samesum_bi_degree[1])
+
+    if abs(Delta_n) > 0:
+        # derive random sample nodes deltai
+        delat_i = random.sample(range(0, n), Delta_n)
+        for i in delat_i:
+            if Delta_n > 0:
+                samesum_bi_degree[1][i] + 1 # D_minus random degree increase 1
+            else:
+                samesum_bi_degree[0][i] + 1 # D_plus random degree increse 1
+        print("lxh")
+
+    return samesum_bi_degree
+
+
+
+
 
 
 
