09fe9183b343707c3e21817ccccc976682751084
luhuan wu <leah@luhuans-MacBook-Pro.local>
2017-08-01 09:28:36 +0800

    add wiki_Vote

--- RealWorldGraph.py
+++ RealWorldGraph.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import networkx as nx
 from AnalysisToolFunction import test_shortpath_marginal_2, plot_ave
+import scipy.stats as st
+import DCMGenerator as dcm_g
+from week_6 import d_tolist
 
 data = pd.read_csv('wiki-Vote.txt', header=3, sep='\t')
 data.columns=['FromNodeId', 'ToNodeId']
@@ -15,3 +18,12 @@ graph.add_edges_from(edges)
 
 
 result = test_shortpath_marginal_2(graph, 5)
+
+din = list(graph.in_degree().values())
+dout = list(graph.out_degree().values())
+
+corr, p = st.pearsonr(din, dout)
+
+# dict
+pr = nx.pagerank(graph)
+bc = nx.betweenness_centrality_source(graph)
